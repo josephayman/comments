5ace18d2a660996f838f3bee70a21b17d6b3fe71
Joseph Ayman <josephayman@mail.com>
2022-12-01 16:37:46 +0100

    Added Dashboard with empty state

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { Button, Heading, Text, Icon, Box } from '@chakra-ui/react'
+import { Button, Heading, Text, Icon, Box, Flex } from '@chakra-ui/react'
 import { Logo, Github, Google } from '../styles/icons'
+import EmptyState from "../components/EmptyState";
 
 
 export default function Home() {
@@ -10,29 +11,27 @@ export default function Home() {
   const supabase = useSupabaseClient();
   const user = session?.user.user_metadata;
   return (
-    <div>
+    <Flex direction="column" align="center" justify="center" h="100vh">
 
-      <main>
         <Heading>
-          Welcome to Comments
         </Heading>
 
-        <Logo  />
+        <Icon as={Logo} w={10} h={10} />  
 
         <Box as="main">
           {!session ? (
-              <Button leftIcon={<Github />} onClick={() => supabase.auth.signInWithOAuth({ provider: "github" })}>Sign in with GitHub</Button>
+              <Button mt={4} leftIcon={<Github />} onClick={() => supabase.auth.signInWithOAuth({ provider: "github" })}>Sign in with GitHub</Button>
           ) : (
             <>            
-            <Text>Logged in as {user.email}</Text>
-            <Button onClick={() => supabase.auth.signOut()}>Sign out</Button>
+            <Text mt={4} >Logged in as {user.email}</Text>
+            <Button mt={4} onClick={() => supabase.auth.signOut()}>Sign out</Button>
+            <EmptyState />  
             </> 
           )}
         </Box>
-      </main>
 
       <footer>
       </footer>
-    </div>
+    </Flex>
   );
 }

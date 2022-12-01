e3e1fafc633be71c3f445ce20cb0bd6b2a16b6f0
Joseph Ayman <josephayman@mail.com>
2022-12-01 14:13:52 +0100

    Added chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,26 @@
-import "../styles/globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../styles/theme";
+import "../styles/globals.css";
+
 import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+
   return (
-  <SessionContextProvider
-    supabaseClient={supabaseClient}
-    initialSession={pageProps.initialSession}
-  >
-    <Component {...pageProps} />
-  </SessionContextProvider>
+    <ChakraProvider theme={theme} resetCSS={true} >
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
   );
 }
 
 export default MyApp;
-
